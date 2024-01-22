1bdce76c41e5aa3fc7f0b42b53ab548b269a9480
Sunatullo Dustov <114532733+dustov2003@users.noreply.github.com>
2024-01-22 03:21:37 +0300

    Add files via upload

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": "airbnb-base",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '*.config.js',
+    '*.test.js',
+  ],
+  rules: {},
+};
